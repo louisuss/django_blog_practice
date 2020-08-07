a578c27b14f98dbdbc957f2035385c38ce0cdd13
louis <dobi1115@gmail.com>
2020-08-07 22:36:33 +0900

    Added add, change, update, delete urls in blog and bookmark urls

--- blog/urls.py
+++ blog/urls.py
@@ -38,4 +38,12 @@ urlpatterns = [
          name='tagged_object_list'),
     # /blog/search/
     path('search/', views.SearchFormView.as_view(), name='search'),
+    # /blog/add/
+    path('add/', views.PostCreateView.as_view(), name='add'),
+    # /blog/change/
+    path('change/', views.PostChangeLV.as_view(), name='change'),
+    # /blog/99/update/
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    # /blog/99/delete/
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
 ]
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from bookmark.views import BookmarkDV, BookmarkLV
+# from bookmark.views import BookmarkDV, BookmarkLV
+from bookmark import views
 
 # 애플리케이션 네임스페이스 : URL 패턴의 이름을 정하는 데 사용
 app_name = 'bookmark'
 
 
 urlpatterns = [
-    path('', BookmarkLV.as_view(), name='index'),
-    path('<int:pk>/', BookmarkDV.as_view(), name='detail'),
+    path('', views.BookmarkLV.as_view(), name='index'),
+    path('<int:pk>/', views.BookmarkDV.as_view(), name='detail'),
+    # /bookmark/add/
+    path('add/', views.BookmarkCreateView.as_view(), name='add'),
+    # /bookmark/change/
+    path('change/', views.BookmarkChangeLV.as_view(), name='change'),
+    # /bookmark/99/update/
+    path('<int:pk>/update/', views.BookmarkUpdateView.as_view(), name='update'),
+    # /bookmark/99/delete/
+    path('<int:pk>/delete/', views.BookmarkDeleteView.as_view(), name='delete'),
 ]
