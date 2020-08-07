5b58056a3defd7c0e55296b3356570f7edacc221
louis <dobi1115@gmail.com>
2020-08-07 22:28:30 +0900

    Added owner field in blog and bookmark models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse  # URL 패턴 만들어줌
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils.text import slugify  # slug 필드 자동 채움
 
 
 class Post(models.Model):
@@ -14,6 +16,8 @@ class Post(models.Model):
     create_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
     modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
     tags = TaggableManager(blank=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         verbose_name = 'post'
@@ -32,3 +36,7 @@ class Post(models.Model):
 
     def get_next(self):
         return self.get_next_by_modify_dt()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
--- bookmark/models.py
+++ bookmark/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ from django.db import models
 class Bookmark(models.Model):
     title = models.CharField('TITLE', max_length=100, blank=True)
     url = models.URLField('URL', unique=True)
+    # N:1 관계 -> 외래키 활용
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
