9059c31f964e6816aed951b2258d765237905d59
louis <dobi1115@gmail.com>
2020-08-08 21:05:43 +0900

    Added create, change, update, delete urls

--- photo/urls.py
+++ photo/urls.py
@@ -12,4 +12,22 @@ urlpatterns = [
     path('album/<int:pk>/', views.AlbumDV.as_view(), name='album_detail'),
     # /photo/photo/99/
     path('photo/<int:pk>/', views.PhotoDV.as_view(), name='photo_detail'),
+    # /photo/album/add/
+    path('album/add/', views.AlbumPhotoCV.as_view(), name='album_add'),
+    # /photo/album/change/
+    path('album/change/', views.AlbumChangeLV.as_view(), name='album_change'),
+    # /photo/album/99/update
+    path('album/<int:pk>/update/',
+         views.AlbumPhotoUV.as_view(), name='album_update'),
+    # /photo/album/99/delete
+    path('album/album/<int:pk>/update/',
+         views.AlbumDelV.as_view(), name='album_delete'),
+    # /photo/photo/add/
+    path('photo/add/', views.PhotoCV.as_view(), name='photo_add'),
+    # /photo/photo/change/
+    path('photo/change/', views.PhotoUV.as_view(), name='photo_change'),
+    # /photo/photo/99/update/
+    path('photo/<int:pk>/update/', views.PhotoUV.as_view(), name='photo_update'),
+    # /photo/photo/99/delete/
+    path('photo/<int:pk>/delete/', views.PhotoDelV.as_view(), name='photo_delete'),
 ]
